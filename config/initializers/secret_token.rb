65ce43d386ac3ae5df83ca79936d1e67fca36899
Courtland Caldwell <courtland.caldwell@cympel.com>
2014-05-18 13:13:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '646b819c72a943a9742e091de469e85b6f6550de6b82ea921803ade6feb39b1d587125a187f174188e7dcccdddd17e9efde7e534676e97d947ab4c53d909557d'
